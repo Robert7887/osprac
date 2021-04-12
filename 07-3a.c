70618873b42662b68b61b800612596323fbbf450
Robert <mkrtumyan_robert@mail.ru>
2021-04-12 23:48:41 +0300

    fix second task

--- 07-3a.c
+++ 07-3a.c
@@ -51,40 +51,42 @@ int main() {
     exit(-1);
   }
 
-  if ((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0) {
 
-    printf("Can\'t create semaphore set\n");
-
-    exit(-1);
-  } else {
+  if ((semid = semget(key, 1, 0666)) < 0) {
+    if ((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0) {
+      printf("Can\'t create semaphore set\n");
+      exit(-1);
+    }
     mybuf.sem_num = 0;
     mybuf.sem_op  = 1;
     mybuf.sem_flg = 0;
     semop(semid, &mybuf, 1);
   }
 
+
+  mybuf.sem_num = 0;
+  mybuf.sem_op  = -1;
+  mybuf.sem_flg = 0;
+  semop(semid, &mybuf, 1);
+  
   if (new) {
     array[0] =  1;
     array[1] =  0;
     array[2] =  1;
   } else {
-    mybuf.sem_num = 0;
-    mybuf.sem_op  = -1;
-    mybuf.sem_flg = 0;
-    semop(semid, &mybuf, 1);
-
     array[0] += 1;
     for(i=0; i<2000000000L; i++);
     array[2] += 1;
-
-    mybuf.sem_num = 0;
-    mybuf.sem_op  = 1;
-    mybuf.sem_flg = 0;
-    semop(semid, &mybuf, 1);
   }
+
 
   printf("Program 1 was spawn %d times, program 2 - %d times, total - %d times\n",
     array[0], array[1], array[2]);
+
+  mybuf.sem_num = 0;
+  mybuf.sem_op  = 1;
+  mybuf.sem_flg = 0;
+  semop(semid, &mybuf, 1);
 
   if (shmdt(array) < 0) {
 
